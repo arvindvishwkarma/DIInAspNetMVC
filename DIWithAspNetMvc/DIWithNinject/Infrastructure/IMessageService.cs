a9cbf56894b44c158e5131a41d3c27e861bfba22
Arvind <arvindvmail@gmail.com>
2013-12-22 10:11:42 +0530

    Removed unused using's.

--- DIWithAspNetMvc/DIWithNinject/Infrastructure/IMessageService.cs
+++ DIWithAspNetMvc/DIWithNinject/Infrastructure/IMessageService.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace DIWithNinject.Infrastructure
+﻿namespace DIWithNinject.Infrastructure
 {
     public interface IMessageService
     {
--- DIWithAspNetMvc/DIWithNinject/Services/MessageService.cs
+++ DIWithAspNetMvc/DIWithNinject/Services/MessageService.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using DIWithNinject.Infrastructure;
+﻿using DIWithNinject.Infrastructure;
 
 namespace DIWithNinject.Services
 {
